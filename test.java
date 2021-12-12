2f6b6dce46bf21242cac70a855d2a7bf4ebd16c2
MatthewJohnson07 <matthewjohnson101@u.boisestate.edu>
2021-12-11 17:04:27 -0700

    Method added

--- test.java
+++ test.java
@@ -2,5 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("This is a message");
         System.out.println("This is another message");
+        method();
+    }
+
+    public void method(){
+        System.out.println("Method 1");
     }
 }
