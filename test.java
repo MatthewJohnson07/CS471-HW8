cad51bf2af3d8e715e4a19f08522ea2bed9493e9
MatthewJohnson07 <matthewjohnson101@u.boisestate.edu>
2021-12-11 17:03:41 -0700

    Add Print Statements

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        
+        System.out.println("This is a message");
+        System.out.println("This is another message");
     }
 }
